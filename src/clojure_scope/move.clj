5988becbe2fd4b68b8acbf55a28194572efeb1d6
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-04-22 16:34:23 +0300

    clarify docstring

--- src/clojure_scope/move.clj
+++ src/clojure_scope/move.clj
@@ -1,7 +1,11 @@
 (ns clojure-scope.move)
 
 (defn move-vars
-  "moves given vars to the given namespace and updates call sites and the ns form"
-  [vars target-namespace]
+  "moves given vars to the given namespace and updates call sites and
+  adds new aliases to the ns forms. vars are vectors in
+  form [\"namespace\" \"name\"] The file corresponding the target
+  namespace must exist. The vars are appended to the target namespace
+  file in the order they are given."
+  [source-folder vars target-namespace]
 
   )
