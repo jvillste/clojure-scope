8d0ccf55b92b1f6b5818e5ffd074d67bbb9a4651
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-05-21 18:01:18 +0300

    rename test

--- src/clojure_scope/core.clj
+++ src/clojure_scope/core.clj
@@ -107,11 +107,11 @@
 (defn dependents-by-var [dependency-graph]
   (group-by-pair dependency-graph :dependency :dependent))
 
-(deftest dependents-by-var-test
+(deftest test-dependents-by-var
   (is (= {["ns" "b"] [["ns" "a"]]
           ["ns" "a"] [["ns" "c"]]}
          (dependents-by-var
-          [{:dependent ["ns" "a"], :dependency ["ns" "b"]}
+          [{:dependent ["ns" "ax"], :dependency ["ns" "b"]}
            {:dependent ["ns" "c"], :dependency ["ns" "a"]}]))))
 
 (defn immediate-dependents [dependency-graph var]
